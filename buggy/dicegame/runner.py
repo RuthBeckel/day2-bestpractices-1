052d00dc1820191b1b73853258d3689c81c83b53
RuthBeckel <ruth.beckel@posteo.de>
2022-02-22 15:27:10 +0100

    Some ugly fixes in the buggy program. If this wasn't just an excerceise, I would re-write the whole thing.

--- buggy/dicegame/runner.py
+++ buggy/dicegame/runner.py
@@ -5,27 +5,25 @@ class GameRunner:
 
     def __init__(self):
         self.dice = Die.create_dice(5)
-        self.reset()
-
-    def reset(self):
-        self.round = 1
-        self.wins = 0
-        self.loses = 0
 
     def answer(self):
         total = 0
         for die in self.dice:
-            total += 1
+            total += die.value # add numbers on dice not number of dices
         return total
+    
 
     @classmethod
     def run(cls):
         # Probably counts wins or something.
         # Great variable name, 10/10.
-        c = 0
+        wins = 0 # counter in main loop
+        looses = 0 # counter in main loop
+        n_round = 1 # counter in main loop
         while True:
             runner = cls()
-
+            
+            runner.round = n_round # actually unnecessary, just an ugly bug fix
             print("Round {}\n".format(runner.round))
 
             for die in runner.dice:
@@ -36,25 +34,37 @@ class GameRunner:
 
             if guess == runner.answer():
                 print("Congrats, you can add like a 5 year old...")
-                runner.wins += 1
-                c += 1
+                wins +=1 # add to counter
+                
             else:
                 print("Sorry that's wrong")
                 print("The answer is: {}".format(runner.answer()))
                 print("Like seriously, how could you mess that up")
-                runner.loses += 1
-                c = 0
+                looses += 1 # add to counter
+                
+            runner.wins = wins # actually unnecessary, just an ugly bug fix
+            runner.loses = looses # actually unnecessary, just an ugly bug fix
             print("Wins: {} Loses {}".format(runner.wins, runner.loses))
-            runner.round += 1
-
-            if c == 6:
-                print("You won... Congrats...")
-                print("The fact it took you so long is pretty sad")
+            
+            if n_round == 3:
+                if wins == n_round:
+                    print("You won... Congrats...")
+                    print("The fact it took you so long is pretty sad")
+                elif wins > looses:
+                    print("You should be able to do better than that.")
+                    print("Go and practice. But not with me. Leave me alone!")
+                else:
+                    print("Serieously, you should go back to primary school.")
                 break
+            
+            n_round += 1 # add to counter
 
             prompt = input("Would you like to play again?[Y/n]: ")
 
-            if prompt == 'y' or prompt == '':
+            if prompt in ['y','Y','']: 
                 continue
+            elif prompt in ['n','N']: 
+                print('Thanks for finally leaving me alone!')
+                break
             else:
                 i_just_throw_an_exception()
